21079241e2923ee688822de3d858c523ab8a143b
telvinmj <telvinmj@gmail.com>
2023-10-14 21:40:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WordCounter from './components/Wordcounter/Wordcounter';
+
+function App() {
+  return (
+    <div className="App">
+      <><WordCounter/></>
+    </div>
+  );
+}
+
+export default App;
